55d7924fdcd200df28167ff779fd4a05dafd08b2
Md. Alahi Almin Tansen <aatansen@gmail.com>
2024-04-15 21:51:47 +0600

    host

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2%+h%d=kn5o-xyhf9u3wedt4glp4jivracxv898u&ikc-&#gll
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
