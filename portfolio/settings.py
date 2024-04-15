f2bba57e75533cb1210cd15ec5c385dd8872499a
Md. Alahi Almin Tansen <aatansen@gmail.com>
2024-04-15 22:18:56 +0600

    host fix

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2%+h%d=kn5o-xyhf9u3wedt4glp4jivracxv898u&ikc-&#gll
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
