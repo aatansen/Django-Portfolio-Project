0ee06a131a27339e906a286823cafbc5e4d4c4f1
Md. Alahi Almin Tansen <aatansen@gmail.com>
2024-04-15 22:59:33 +0600

    pillow added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,4 +128,4 @@ if DEBUG:
         BASE_DIR / "static",
     ]
 else:
-    STATIC_ROOT = BASE_DIR / "static",
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
